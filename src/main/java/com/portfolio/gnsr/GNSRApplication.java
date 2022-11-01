87df3b4a1c1a590c5aed9841c976cb36fd8fd5ad
giannisavid <gianninasavid@gmail.com>
2022-10-31 21:44:13 -0300

    BackEndArgProg

---
+++ src/main/java/com/portfolio/gnsr/GNSRApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.gnsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GNSRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GNSRApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/portfolio/gnsr/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.gnsr.Repository;
+
+import com.portfolio.gnsr.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
